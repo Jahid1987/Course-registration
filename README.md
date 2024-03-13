2722199bcb582c5617c87f1d06339fc129a3ee79
Jahid Anwer <jahid.englaa@gmail.com>
2024-03-13 19:08:15 +0600

    tailwind, daisyUI, fake json data added

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + Tailwindcss + daisyUI
+
+Applying in this course:
+
+1. react's props drilling
+2. state management
+3.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <h1 className="text-3xl font-sans font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
